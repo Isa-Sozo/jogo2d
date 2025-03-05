2f302781b24cd9ec30320e79b0ff5b221609772c
ISADORA SOZO DA SILVA <isadora_sozo-silva@estudante.sesisenai.org.br>
2025-03-05 10:57:39 -0300

    fazendo ele pular

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('keypress', (e) => {
     if(e.code == 'Space'){
         personagem.velocidY =  15
         console.log("PULOU")
-        
+        personagem.pulando = true
     
     }})
 
@@ -15,7 +15,7 @@ const personagem ={
     altura: 50,
     largura: 50,
     velocidY: 0,
-    pulando: true
+    pulando: false
 }
 
 function desenharPersonagem(){
@@ -26,6 +26,10 @@ function atualizarPersonagem(){
     if(personagem.pulando == true){
         personagem.velocidY -= gravidade
         personagem.y -= personagem.velocidY
+        if(personagem.y >= canvas.height-50){
+            personagem.velocidY = 0
+            personagem.pulando = false
+        }
     }
 }
 
