6f25673b66e013bf50a0944a4fea2f09a440f9aa
Isadora Sozo <isadora_sozo-silva@estudante.sesisenai.org.br>
2025-03-12 09:25:14 -0300

    exercicio oop

--- exercicioclasse.js
+++ exercicioclasse.js
@@ -1,34 +1,87 @@
 class Veiculo {
-    constructor( tipo, marca,cor, velocidade, passageiros){
+    #velocidade;
+
+    constructor(tipo, marca, cor, velocidade, passageiros) {
         this.tipo = tipo;
         this.marca = marca;
         this.cor = cor;
-        this.velocidade = velocidade;
+        this.#velocidade = velocidade;
         this.passageiros = passageiros;
     }
-    acelerar = function(){
-        this.velocidade += 10
-        console.log(this.velocidade)
+
+    acelerar() {
+        this.#velocidade += 10;
+        console.log(`Velocidade: ${this.#velocidade} km/h`);
     }
-    freiar = function(){
-        if (this.velocidade > 0){
-            this.velocidade -= 5
-            console.log(this.velocidade)
-        }else{
-            console.log("o carro já está parado")
+
+    freiar() {
+        if (this.#velocidade > 0) {
+            this.#velocidade -= 5;
+            console.log(`Velocidade: ${this.#velocidade} km/h`);
+        } else {
+            console.log("O veículo já está parado.");
+        }
+    }
+
+    setVelocidade(valor) {
+        this.#velocidade = valor;
+    }
+
+    getVelocidade() {
+        return this.#velocidade;
+    }
+}
+
+class Aviao extends Veiculo {
+    constructor(tipo, marca, cor, velocidade, passageiros, companhia) {
+        super(tipo, marca, cor, velocidade, passageiros);
+        this.companhia = companhia;
+    }
+}
+
+class Barco extends Veiculo {
+    constructor(tipo, marca, cor, velocidade, passageiros, pier) {
+        super(tipo, marca, cor, velocidade, passageiros);
+        this.pier = pier;
+    }
+
+    getVelocidadeEmNos() {
+        return (this.getVelocidade() / 1.852).toFixed(2);
+    }
+
+    acelerar() {
+        this.setVelocidade(this.getVelocidade() + 10);
+        console.log(`Velocidade: ${this.getVelocidade()} km/h (${this.getVelocidadeEmNos()} nós)`);
+    }
+
+    freiar() {
+        if (this.getVelocidade() > 0) {
+            this.setVelocidade(this.getVelocidade() - 5);
+            console.log(`Velocidade: ${this.getVelocidade()} km/h (${this.getVelocidadeEmNos()} nós)`);
+        } else {
+            console.log("O barco já está parado.");
         }
     }
 }
-const carro = new Veiculo(
-    'SUV',
-    'renaut',
-    'cinza',
-    0,
-    0
-)
-const outro_carro = new Veiculo('sedan','fiat', 'preto', 0,0)
-outro_carro.acelerar()
-carro.acelerar()
-carro.acelerar()
-carro.freiar()
-carro.freiar()
+
+// Testando as classes
+const carro = new Veiculo('SUV', 'Renault', 'Cinza', 0, 0);
+const outro_carro = new Veiculo('Sedan', 'Fiat', 'Preto', 0, 0);
+outro_carro.acelerar();
+carro.acelerar();
+carro.acelerar();
+carro.freiar();
+carro.freiar();
+
+const aviao = new Aviao('Comercial', 'Boeing', 'Branco', 0, 0, 'Gol');
+aviao.acelerar();
+aviao.freiar();
+console.log(aviao);
+
+const barco = new Barco('Simples', 'De Pesca', 'Branco', 0, 0, 'Marinha');
+barco.acelerar();
+barco.freiar();
+console.log(barco);
+
+aviao.setVelocidade(50);
+console.log(aviao.getVelocidade());
