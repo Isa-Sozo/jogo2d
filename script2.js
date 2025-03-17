490f049f827d406660822b74b4746c3cc72db790
Isadora Sozo <isadora_sozo-silva@estudante.sesisenai.org.br>
2025-03-17 07:40:58 -0300

    atualização código

--- script2.js
+++ script2.js
@@ -3,9 +3,9 @@ const ctx = canvas.getContext('2d');
 let jogoAtivo = true;
 
 document.addEventListener('keypress', (e) => {
-    if (e.code == 'Space' && !personagem.pulando && jogoAtivo) {
+    if (e.code === 'Space' && !personagem.pulando && jogoAtivo) {
         personagem.saltar();
-    } else if (e.code == 'Enter' && !jogoAtivo) {
+    } else if (e.code === 'Enter' && !jogoAtivo) {
         reiniciarJogo();
     }
 });
@@ -35,13 +35,14 @@ class Personagem extends Entidade {
         super(x, y, largura, altura);
         this.#pulando = false;
         this.#velocidadey = 0;
+        this.img = new Image();
+        this.img.src = 'monsterhigh-morcego.png';
     }
 
     saltar() {
         if (!this.#pulando) {
             this.#velocidadey = 15;
             this.#pulando = true;
-            console.log('saltou');
         }
     }
 
@@ -53,29 +54,55 @@ class Personagem extends Entidade {
         if (this.#pulando) {
             this.#velocidadey -= this.gravidade;
             this.y -= this.#velocidadey;
-
-            if (this.y >= canvas.height - 50) {
+            if (this.y >= canvas.height - this.altura) {
                 this.#velocidadey = 0;
                 this.#pulando = false;
-                this.y = canvas.height - 50;
+                this.y = canvas.height - this.altura;
             }
         }
+    }
+
+    desenhar() {
+        ctx.drawImage(this.img, this.x, this.y, this.largura, this.altura);
     }
 }
 
 class Obstaculo extends Entidade {
-    constructor(x, y, largura, altura) {
+    constructor(x, y, largura, altura, velocidade) {
         super(x, y, largura, altura);
+        this.velocidade = velocidade;
+    }
+
+    mover() {
+        this.x -= this.velocidade;
+        if (this.x + this.largura < 0) {
+            this.x = canvas.width;
+            this.velocidade += 0.2;
+            let nova_altura = (Math.random() * 50) + 100;
+            this.altura = nova_altura;
+            this.y = canvas.height - nova_altura;
+        }
     }
 }
-const obstaculo = new Obstaculo (100, canvas.height - 100, 100, 50)
 
 const personagem = new Personagem(100, canvas.height - 50, 50, 50);
+const obstaculo = new Obstaculo(canvas.width, canvas.height - 100, 50, 100, 7);
+
+function verificarColisao() {
+    if (
+        personagem.x < obstaculo.x + obstaculo.largura &&
+        personagem.x + personagem.largura > obstaculo.x &&
+        personagem.y < obstaculo.y + obstaculo.altura &&
+        personagem.y + personagem.altura > obstaculo.y
+    ) {
+        jogoAtivo = false;
+    }
+}
 
 function exibirGameOver() {
     ctx.fillStyle = 'hsla(342, 96.90%, 62.50%, 0.70)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    
     ctx.fillStyle = 'BEIGE';
     ctx.font = 'bold 50px Arial';
     ctx.textAlign = 'center';
@@ -86,19 +113,33 @@ function exibirGameOver() {
     ctx.font = '20px Arial';
     ctx.fillStyle = 'white';
     ctx.shadowBlur = 0;
-    if (Math.floor(Date.now() / 500) % 2 === 0) { 
+    if (Math.floor(Date.now() / 500) % 2 === 0) {
         ctx.fillText('Pressione ENTER para reiniciar', canvas.width / 2, canvas.height / 2 + 40);
     }
-
+    
     requestAnimationFrame(exibirGameOver);
+}
+
+function reiniciarJogo() {
+    jogoAtivo = true;
+    personagem.y = canvas.height - 50;
+    personagem.velocidadey = 0;
+    personagem.pulando = false;
+    obstaculo.x = canvas.width;
+    obstaculo.velocidade = 7;
+    loop();
 }
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     if (jogoAtivo) {
-        personagem.desenhar("black");
+        personagem.desenhar();
         personagem.atualizarPersonagem();
+        
+        obstaculo.desenhar("rgb(52,42,133)");
+        obstaculo.mover();
+        
+        verificarColisao();
         requestAnimationFrame(loop);
     } else {
         exibirGameOver();
